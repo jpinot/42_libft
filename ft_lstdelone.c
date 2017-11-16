a216de87564b61ae39bedddf4da21a0b6da19de2
Josep PINYOT <jpinyot@e2r9p8.42.fr>
2017-11-17 00:30:49 +0100

    test non -lstiter

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/16 20:35:46 by jpinyot           #+#    #+#             */
-/*   Updated: 2017/11/16 20:35:50 by jpinyot          ###   ########.fr       */
+/*   Created: 2017/11/16 18:42:12 by jpinyot           #+#    #+#             */
+/*   Updated: 2017/11/16 18:42:30 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	if (lst)
-	{
-		while (lst)
-		{
-			f(lst);
-			lst = lst->next;
-		}
-	}
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
 }
