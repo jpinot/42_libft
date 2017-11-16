bc21ca93b4f595d99918dd909f9284e1d2b0fadd
Josep PINYOT <jpinyot@e2r9p8.42.fr>
2017-11-17 00:29:50 +0100

    test non -lstdelone

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/16 18:42:12 by jpinyot           #+#    #+#             */
-/*   Updated: 2017/11/16 18:42:30 by jpinyot          ###   ########.fr       */
+/*   Created: 2017/11/16 18:42:04 by jpinyot           #+#    #+#             */
+/*   Updated: 2017/11/16 20:47:12 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
+	t_list *tmp;
+	t_list *lst;
+
+	lst = *alst;
+	while (lst)
+	{
+		tmp = lst->next;
+		del(lst->content, lst->content_size);
+		free(lst);
+		lst = tmp;
+	}
 	*alst = NULL;
 }
